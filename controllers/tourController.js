64fb082c3f287909defaf58b950a03494925fe1c
wwwam <amannakoti0@gmail.com>
2026-02-24 21:19:36 +0530

    database using catch

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,7 @@
 const Tour = require('../models/tourModel')
 
-//! CREAETE A CHECKBODY MIDDLEWARE 
-//! CHECK IF BODY CONTAINS THE NAME AND PRICE PROPERTY
-//! IF NOT SEND 400 BACK TO CLIENT
-//! ADD IT POST HANDLER STACK
-
 exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
+
 
   res.status(200).json({
     status: 'success',
@@ -32,14 +27,24 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  // console.log(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour
-    }
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
+
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    })
+  }
 };
 
 exports.updateTour = (req, res) => {
